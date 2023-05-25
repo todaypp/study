231e08cdc1fe2fd1537d04b932103cb49f79dbb4
Sumin Son <clvswft03@gmail.com>
2023-05-26 06:27:39 +0900

    fix(devops_gradle): fix build problems (jvm 8 => 11)

--- development/devops/gradle/sample_structuring_software_projects-kotlin-dsl/build-logic/commons/src/main/kotlin/com.example.commons.gradle.kts
+++ development/devops/gradle/sample_structuring_software_projects-kotlin-dsl/build-logic/commons/src/main/kotlin/com.example.commons.gradle.kts
@@ -1,23 +1,23 @@
 plugins {
-    id("java")
-    id("com.example.jacoco")
+  id("java")
+  id("com.example.jacoco")
 }
 
 group = "com.example.myproduct"
 
 java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
-    }
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(11))
+  }
 }
 
 dependencies {
-    implementation(platform("com.example.platform:product-platform"))
+  implementation(platform("com.example.platform:product-platform"))
 
-    testImplementation(platform("com.example.platform:test-platform"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation(platform("com.example.platform:test-platform"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
